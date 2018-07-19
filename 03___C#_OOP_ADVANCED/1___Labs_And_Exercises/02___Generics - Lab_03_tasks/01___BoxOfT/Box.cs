67cc56d4e0888d080299d9ee58dd735b02cd086b
Petko Petkov <ptpetkov666@gmail.com>
2018-07-19 10:56:03 +0300

    '/f/VS/GITHUB/Generics - Lab_03_tasks(first task)'

---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/02___Generics - Lab_03_tasks/01___BoxOfT/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Box<T>
+{
+    private readonly List<T> items;
+    public int Count => this.items.Count;
+    
+    public Box()
+    {
+        this.items = new List<T>();
+    }
+    public void Add(T element)
+    {
+        items.Add(element);
+    }
+    public T Remove()
+    {
+        var element = items.Last();
+        items.RemoveAt(this.items.Count - 1);
+        return element;
+    }
+    
+}
+
---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/02___Generics - Lab_03_tasks/01___BoxOfT/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        var box = new Box<int>();
+        box.Add(1);
+        box.Add(2);
+        box.Add(3);
+        box.Add(4);
+        box.Add(5);
+        var element = box.Remove();
+        Console.WriteLine(element);
+        Console.WriteLine(box.Count);
+
+    }
+}
+
