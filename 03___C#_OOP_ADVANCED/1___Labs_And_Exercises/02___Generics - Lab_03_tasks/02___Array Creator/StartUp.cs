f0a6068681ad685149460f4b86b9d34b6253280a
Petko Petkov <ptpetkov666@gmail.com>
2018-07-20 12:30:16 +0300

    Generics Lab -  second task

---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/02___Generics - Lab_03_tasks/02___Array Creator/StartUp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        string[] strings = ArrayCreator.Create(5, "pesho");
+        int[] integers = ArrayCreator.Create(10, 33);
+        
+    }
+}
+
