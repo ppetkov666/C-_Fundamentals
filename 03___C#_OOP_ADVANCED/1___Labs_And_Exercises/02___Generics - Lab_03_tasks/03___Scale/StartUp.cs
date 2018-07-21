361af347dfffa20aaa6b1be9cc2d8ea673b92510
Petko Petkov <ptpetkov666@gmail.com>
2018-07-21 11:41:43 +0300

    Generics - third task

---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/02___Generics - Lab_03_tasks/03___Scale/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+       int firstNumTest = 123345346;
+       int secondNumTest = 6789567;
+       string nameTest = "petko";
+       string nameAnotherTest = "petkopetkov";
+       
+       var myScale = new Scale<int>(firstNumTest, secondNumTest);
+       Console.WriteLine(myScale.GetHeavier());
+       var myScalesecond = new Scale<string>(nameTest, nameAnotherTest);
+       Console.WriteLine(myScalesecond.GetHeavier());
+    }
+
+}
+
+
