8bcd61403d834a9b90c787c9ad71188b95a7700c
Petko Petkov <ptpetkov666@gmail.com>
2018-07-23 13:15:34 +0300

    Generics - Exercise_11_tasks

---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/02___Generics - Exercise_11_tasks/01___Generic Box of String/Box.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Box<T>
+{
+    public T value;
+
+    public Box(T value)
+    {
+        this.value = value;
+    }
+    public override string ToString()
+    {
+        return $"{this.value.GetType().FullName}: {this.value}";
+    }
+}
+
+
+
