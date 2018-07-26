f7570727d9d100f56b7e17f9fb2a1c0c63dca14a
Petko Petkov <ptpetkov666@gmail.com>
2018-07-26 15:11:25 +0300

    Generics - exercise - task 2

---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/02___Generics - Exercise_11_tasks/02___Generic Box of Integer/Box.cs
@@ -0,0 +1,31 @@
+﻿namespace _02___Generic_Box_of_Integer
+{
+    using System;
+
+    public class Box<T> : IComparable<Box<T>>
+        where T : IComparable
+    {
+        public Box()
+        {
+            this.Value = default(T);
+        }
+
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public int CompareTo(Box<T> other)
+        {
+            return this.Value.CompareTo(other.Value);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Value.GetType().FullName}: {this.Value}";
+        }
+    }
+
+}
---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/02___Generics - Exercise_11_tasks/02___Generic Box of Integer/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace _02___Generic_Box_of_Integer
+{
+    using System;
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var numOfStrings = int.Parse(Console.ReadLine());
+
+            while (numOfStrings > 0)
+            {
+                Console.WriteLine(new Box<int>(int.Parse(Console.ReadLine())));
+
+                numOfStrings--;
+            }
+        }
+    }
+}
