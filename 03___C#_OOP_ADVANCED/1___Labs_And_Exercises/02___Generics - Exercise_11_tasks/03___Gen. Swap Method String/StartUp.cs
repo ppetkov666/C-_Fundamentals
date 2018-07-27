0abbf760ff9170c0b2011f1b71780781a5c61f81
Petko Petkov <ptpetkov666@gmail.com>
2018-07-27 13:26:35 +0300

    Generics - exercise 2 tasks

---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/02___Generics - Exercise_11_tasks/03___Gen. Swap Method String/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace _03___Gen._Swap_Method_String
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var numOfStrings = int.Parse(Console.ReadLine());
+            var listOfBoxes = new List<Box<string>>();
+
+            for (int i = 0; i < numOfStrings; i++)
+            {
+                var value = Console.ReadLine();
+                listOfBoxes.Add(new Box<string>(value));
+            }
+
+            var swapIndexes = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            Swap(listOfBoxes, swapIndexes[0], swapIndexes[1]);
+            // -- first option 
+            //listOfBoxes.ForEach(s => Console.WriteLine(s));
+            // -- second option
+            foreach (var box in listOfBoxes)
+            {
+                Console.WriteLine(box);
+            }
+        }
+        private static void Swap<T>(IList<T> collection, int indexToSwap, int swapWithIndex)
+        {
+            var temp = collection[indexToSwap];
+            collection[indexToSwap] = collection[swapWithIndex];
+            collection[swapWithIndex] = temp;
+        }
+    }
+}
---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/02___Generics - Exercise_11_tasks/04___Generic Swap Method Int/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace _04___Generic_Swap_Method_Int
+{
+    using _03___Gen._Swap_Method_String;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var numOfstrings = int.Parse(Console.ReadLine());
+            var listOfBoxes = new List<Box<int>>();
+
+            for (int i = 0; i < numOfstrings; i++)
+            {
+                var value = int.Parse(Console.ReadLine());
+                listOfBoxes.Add(new Box<int>(value));
+            }
+
+            var swapIndexes = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            Swap(listOfBoxes, swapIndexes[0], swapIndexes[1]);
+            // -- first option 
+            //listOfBoxes.ForEach(s => Console.WriteLine(s));
+            // -- second option
+            foreach (var box in listOfBoxes)
+            {
+                Console.WriteLine(box);
+            }
+        }
+        private static void Swap<T>(IList<T> collection, int indexToSwap, int swapWithIndex)
+        {
+            var temp = collection[indexToSwap];
+            collection[indexToSwap] = collection[swapWithIndex];
+            collection[swapWithIndex] = temp;
+        }
+    }
+}
