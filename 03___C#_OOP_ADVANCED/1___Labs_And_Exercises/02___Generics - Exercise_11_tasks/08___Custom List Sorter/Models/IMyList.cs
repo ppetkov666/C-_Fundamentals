56243158556ebed0f3918d478fb1ffc217f38d8f
Petko Petkov <ptpetkov666@gmail.com>
2018-07-30 12:02:09 +0300

    Generics exercise - another task

---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/02___Generics - Exercise_11_tasks/08___Custom List Sorter/Models/IMyList.cs
@@ -0,0 +1,23 @@
+﻿namespace _08___Custom_List_Sorter.Models
+{
+    using System.Collections.Generic;
+    
+    public interface IMyList<T> : IEnumerable<T>
+    {
+        void Add(T element);
+
+        T Remove(int index);
+
+        bool Contains(T element);
+
+        void Swap(int index1, int index2);
+
+        int CountGreaterThan(T element);
+
+        T Max();
+
+        T Min();
+
+        void Sort();
+    }
+}
---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/02___Generics - Exercise_11_tasks/08___Custom List Sorter/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace _08___Custom_List_Sorter
+{
+    using Controllers;
+    class StartUp
+    {
+        static void Main()
+        {
+            var engine = new Engine();
+            engine.Run();
+        }
+    }
+}
