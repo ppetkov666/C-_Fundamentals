1fabeeafffbf403f528a0a2f94ad8d3e68ae82d9
Petko Petkov <ptpetkov666@gmail.com>
2018-08-01 13:18:20 +0300

    another task - N7

---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/02___Generics - Exercise_11_tasks/07___Custom List/Controllers/Engine.cs
@@ -0,0 +1,26 @@
+﻿namespace _07___Custom_List.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    class Engine
+    {
+        private CommandInterpreter commandInterprete;
+
+        public Engine()
+        {
+            this.commandInterprete = new CommandInterpreter();
+        }
+
+        public void Run()
+        {
+            var cmdArgs = Console.ReadLine().Split();
+
+            while (cmdArgs[0] != "END")
+            {
+                this.commandInterprete.InterpretCommand(cmdArgs);
+                cmdArgs = Console.ReadLine().Split();
+            }
+        }
+    }
+}
---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/02___Generics - Exercise_11_tasks/07___Custom List/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace _07___Custom_List
+{
+    using System;
+    using Controllers;
+    class StartUp
+    {
+        static void Main()
+        {
+            var engine = new Engine();
+            engine.Run();
+        }
+    }
+}
