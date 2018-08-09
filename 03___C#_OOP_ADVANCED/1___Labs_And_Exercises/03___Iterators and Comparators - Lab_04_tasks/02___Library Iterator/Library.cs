0f3ab945e0d88c7193d43953516efad103c3fd0b
Petko Petkov <ptpetkov666@gmail.com>
2018-08-09 11:22:44 +0300

    Iterators and Comparators - Lab_04_tasks

---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/03___Iterators and Comparators - Lab_04_tasks/02___Library Iterator/Library.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+public class Library:IEnumerable<Book>
+{
+    private List<Book> books;
+    public Library(params Book[] books)
+    {
+        this.books = new List<Book>(books);
+    }
+
+    public IEnumerator<Book> GetEnumerator()
+    {
+       return new LibraryIterator(books);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+    private class LibraryIterator : IEnumerator<Book>
+    {
+        
+
+        private IReadOnlyList<Book> books;
+        private int index;
+        public LibraryIterator(IReadOnlyList<Book> books)
+        {
+            this.books = books;
+            this.index = -1;
+        }
+        public Book Current => books[index];
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+            
+        }
+
+        public bool MoveNext()
+        {
+            index++;
+            return index < books?.Count;
+        }
+
+        public void Reset()
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
+
---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/03___Iterators and Comparators - Lab_04_tasks/04___Book Comparer/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace _04___Book_Comparer
+{
+    using System;
+    class StartUp
+    {
+        static void Main()
+        {
+            Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+            Book bookThree = new Book("The Documents in the Case", 1930);
+            var library = new Library(bookOne, bookTwo, bookThree);
+            foreach (var item in library)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
