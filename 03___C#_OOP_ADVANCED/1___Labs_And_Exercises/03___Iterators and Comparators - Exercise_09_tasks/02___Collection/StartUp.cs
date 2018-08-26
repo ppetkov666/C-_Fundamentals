9c6ed55a36c3de43a4d460d685a2c81299f7b916
Petko Petkov <ptpetkov666@gmail.com>
2018-08-26 13:05:08 +0300

    adding two task to Iterators and Comparators - Exercise

---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/03___Iterators and Comparators - Exercise_09_tasks/02___Collection/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace _02___Collection
+{
+    using System;
+    class StartUp
+    {
+        static void Main()
+        {
+            // this task is actually included in the first task with implemented Interface IEnumerable<T>!
+        }
+    }
+}
---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/03___Iterators and Comparators - Exercise_09_tasks/03___Stack/StartUp.cs
@@ -0,0 +1,59 @@
+﻿namespace _03___Stack
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var stack = new Stack<int>();
+            ExecuteCommands(stack);
+        }
+
+        private static void ExecuteCommands(Stack<int> stack)
+        {
+            var cmdArgs = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            while (cmdArgs[0] != "END")
+            {
+                switch (cmdArgs[0])
+                {
+                    case "Push":
+                        var elements = cmdArgs.Skip(1).Select(int.Parse);
+                        stack.Push(elements);
+                        break;
+                    case "Pop":
+                        try
+                        {
+                            stack.Pop();
+                        }
+                        catch (ArgumentException exeption)
+                        {
+                            Console.WriteLine(exeption.Message);
+                        }
+
+                        break;
+                    default:
+                        break;
+                }
+
+                cmdArgs = Console.ReadLine()
+                    .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            PrintStack(stack);
+            PrintStack(stack);
+        }
+
+        private static void PrintStack(Stack<int> stack)
+        {
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
