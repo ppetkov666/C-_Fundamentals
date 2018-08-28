7524c3e0f4f1da9de8337fe1658a8785b120b109
Petko Petkov <ptpetkov666@gmail.com>
2018-08-28 16:34:22 +0300

    Iterators and comparators - exercise - task 4

---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/03___Iterators and Comparators - Exercise_09_tasks/04___Froggy/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace _04___Froggy
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            LetTheGameBegin();
+        }
+
+        private static void LetTheGameBegin()
+        {
+            var stoneValues = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse);
+            //Stones is gene
+            Stones<int> stones = new Stones<int>(stoneValues);
+
+            Console.WriteLine(string.Join(", ", stones));
+        }
+    }
+}
