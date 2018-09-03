7324ab37f527262bc6da937642a793789665234b
Petko Petkov <ptpetkov666@gmail.com>
2018-09-03 19:52:37 +0300

    task 6 from iterators and comparators exercises

---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/03___Iterators and Comparators - Exercise_09_tasks/06___Strategy Pattern/Comparators/PersonAgeComparator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06___Strategy_Pattern.Comparators
+{
+    using Models;
+    using System.Collections.Generic;
+
+    public class PersonAgeComparator : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            return x.Age - y.Age;
+        }
+    }
+}
