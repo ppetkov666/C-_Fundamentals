6800fde7277c9f0ff1705d1cea496ae61f3024c8
Petko Petkov <ptpetkov666@gmail.com>
2018-09-29 21:04:36 +0300

    task N 9 from iterators and comparators

---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/03___Iterators and Comparators - Exercise_09_tasks/09___Linked List Traversal/StartUp.cs
@@ -0,0 +1,47 @@
+﻿namespace _09___Linked_List_Traversal
+{
+    using System;
+    using System.Text;
+    using Models;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var linkedList = new LinkedList<int>();
+            ExecCommand(linkedList);
+            PrintFinalResult(linkedList);
+        }
+
+        private static void PrintFinalResult(LinkedList<int> linkedList)
+        {
+            Console.WriteLine(linkedList.Count);
+            Console.WriteLine(string.Join(" ", linkedList));
+        }
+
+        private static void ExecCommand(LinkedList<int> linkedList)
+        {
+            var numOfCmd = int.Parse(Console.ReadLine());
+
+            while (numOfCmd > 0)
+            {
+                var cmd = Console.ReadLine().Split();
+                var number = int.Parse(cmd[1]);
+
+                switch (cmd[0])
+                {
+                    case "Add":
+                        linkedList.Add(number);
+                        break;
+                    case "Remove":
+                        linkedList.Remove(number);
+                        break;
+                    default:
+                        break;
+                }
+
+                numOfCmd--;
+            }
+        }
+    }
+}
