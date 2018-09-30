20e0544a6a00a3315dcfd8fcf0405ac8a92f4309
Petko Petkov <ptpetkov666@gmail.com>
2018-09-30 23:09:08 +0300

    reflection and attributes - first task

---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/04___Reflection and Attributes - Lab_06_tasks/01___Stealer/Spy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Reflection;
+using System.Linq;
+
+public class Spy
+{
+    public string StealFieldInfo(string classToInvestigate, params string[] fieldsToInvestigate)
+    {
+        var stringbuilder = new StringBuilder
+            ($"Class under investigation: {classToInvestigate}" + Environment.NewLine);
+        var fields = Type
+            .GetType(classToInvestigate)
+            .GetFields(BindingFlags.Instance 
+                     | BindingFlags.Static
+                     | BindingFlags.NonPublic 
+                     | BindingFlags.Public);
+        var instance = Activator.CreateInstance(Type.GetType(classToInvestigate));
+        foreach (var field in fields)
+        {
+            if (fieldsToInvestigate.Contains(field.Name))
+            {
+                stringbuilder.AppendLine($"{field.Name} = {field.GetValue(instance)}");
+            }
+            
+        }
+        return stringbuilder.ToString().Trim();
+    }
+}
+
---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/04___Reflection and Attributes - Lab_06_tasks/01___Stealer/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace _01___Stealer
+{
+    using System;
+    public class Program
+    {
+        static void Main()
+        {
+            var spy = new Spy();
+            var result = spy.StealFieldInfo("Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
