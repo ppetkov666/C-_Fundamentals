b050e88d01782e7e067cd90cf40bc76d9f29e95e
Petko Petkov <ptpetkov666@gmail.com>
2018-10-15 13:24:25 +0300

    reflection and attributes lab - task N2

---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/04___Reflection and Attributes - Lab_06_tasks/02___High Quality Mistakes/Spy.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Reflection;
+using System.Linq;
+
+public class Spy
+{
+    public string StealFieldInfo(string classToInvestigate, params string[] fieldsToInvestigate)
+    {
+        var stringbuilder = new StringBuilder
+            ($"Class under investigation: {classToInvestigate}" + Environment.NewLine);
+        var fields = Type
+            .GetType(classToInvestigate)
+            .GetFields(BindingFlags.Instance 
+                     | BindingFlags.Static
+                     | BindingFlags.NonPublic 
+                     | BindingFlags.Public);
+        var instance = Activator.CreateInstance(Type.GetType(classToInvestigate));
+        foreach (var field in fields)
+        {
+            if (fieldsToInvestigate.Contains(field.Name))
+            {
+                stringbuilder.AppendLine($"{field.Name} = {field.GetValue(instance)}");
+            }
+            
+        }
+        return stringbuilder.ToString().Trim();
+    }
+    public string AnalyzeAcessModifiers(string className)
+    {
+        var sb = new StringBuilder();
+        var targetType = Type.GetType(className);
+
+        var wrongFields = targetType.GetFields(BindingFlags.Instance | BindingFlags.Static |
+            BindingFlags.Public);
+
+        foreach (var field in wrongFields)
+        {
+            sb.AppendLine($"{field.Name} must be private!");
+        }
+
+        var pulbicMethods = targetType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+        var nonpulbicMethods = targetType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+        foreach (var method in nonpulbicMethods.Where(m => m.Name.StartsWith("get")))
+        {
+            sb.AppendLine($"{method.Name} have to be public!");
+        }
+
+        foreach (var method in pulbicMethods.Where(m => m.Name.StartsWith("set")))
+        {
+            sb.AppendLine($"{method.Name} have to be private!");
+        }
+
+        return sb.ToString().Trim();
+    }
+
+}
+
---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/04___Reflection and Attributes - Lab_06_tasks/02___High Quality Mistakes/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace _02___High_Quality_Mistakes
+{
+    using System;
+    class Program
+    {
+        static void Main()
+        {
+            Spy spy = new Spy();
+            string result = spy.AnalyzeAcessModifiers(typeof(Hacker).FullName);
+            Console.WriteLine(result);
+        }
+    }
+}
