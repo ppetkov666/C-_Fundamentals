db76d8dfe8c962a3e7c05590fcb78d3bd80113e4
Petko Petkov <ptpetkov666@gmail.com>
2018-10-17 16:02:14 +0300

    reflection and attributes task N4

---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/04___Reflection and Attributes - Lab_04_tasks/04___Collector/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace _03___Mission_Private_Impossible
+{
+    using System;
+    class Program
+    {
+        static void Main()
+        {
+            Spy spy = new Spy();
+            string result = spy.CollectGettersAndSetters("Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
