c32554cb8c5a45b34f3df3e03cbe97b98226788d
Petko Petkov <ptpetkov666@gmail.com>
2018-10-29 16:40:14 +0200

    another task from reflection and attributes

---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/04___Reflection and Attributes - Exercise_08_tasks/02___Black Box Integer/BlackBoxIntegerTests.cs
@@ -0,0 +1,42 @@
+﻿namespace _02___Black_Box_Integer
+{
+    using System;
+    using System.Text;
+    using System.Linq;
+    using System.Reflection;
+    
+
+    public class BlackBoxIntegerTests
+    {
+        private StringBuilder testResult;
+
+        public BlackBoxIntegerTests()
+        {
+            this.testResult = new StringBuilder();
+        }
+
+        public string Run(Type type)
+        {
+            var classInstance = Activator.CreateInstance(type, true);
+            var methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+            var fields = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            var command = Console.ReadLine().Split('_');
+
+            while (command[0] != "END")
+            {
+                var num = int.Parse(command[1]);
+                methods.FirstOrDefault(p => p.Name == command[0])?.Invoke(classInstance, new object[] { num });
+
+                foreach (var field in fields)
+                {
+                    this.testResult.AppendLine(field.GetValue(classInstance).ToString());
+                }
+
+                command = Console.ReadLine().Split('_');
+            }
+
+            return testResult.ToString().Trim();
+        }
+    }
+}
--- 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/04___Reflection and Attributes - Exercise_08_tasks/02___Black Box Integer/StartUp.cs
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/04___Reflection and Attributes - Exercise_08_tasks/02___Black Box Integer/StartUp.cs
@@ -1,11 +1,13 @@
 ﻿namespace _02___Black_Box_Integer
 {
+    
     using System;
     class StartUp
     {
         static void Main()
         {
-
+            BlackBoxIntegerTests bbit = new BlackBoxIntegerTests();
+            Console.WriteLine(bbit.Run(typeof(BlackBoxInteger)));
         }
     }
 }
