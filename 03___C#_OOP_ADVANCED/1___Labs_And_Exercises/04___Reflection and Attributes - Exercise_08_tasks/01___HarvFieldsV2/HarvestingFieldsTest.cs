8fc1dc34fefc3b4741b00e6f4aaa539a52959d3d
Petko Petkov <ptpetkov666@gmail.com>
2018-11-06 13:50:19 +0200

    reflection and attributes

---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/04___Reflection and Attributes - Exercise_08_tasks/01___HarvFieldsV2/HarvestingFieldsTest.cs
@@ -0,0 +1,63 @@
+﻿namespace _01___Harvesting_Fields
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    public class HarvestingFieldsTest
+    {
+        private StringBuilder strBuilder;
+
+        public HarvestingFieldsTest()
+        {
+            this.strBuilder = new StringBuilder();
+        }
+
+        internal string Run()
+        {
+            var inputCommand = Console.ReadLine();
+            var fields = typeof(HarvestingFields).GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            while (inputCommand != "HARVEST")
+            {
+                switch (inputCommand)
+                {
+                    case "private":
+                        this.AppendFields(fields.Where(f => f.IsPrivate));
+                        break;
+                    case "protected":
+                        this.AppendFields(fields.Where(f => f.IsFamily));
+                        break;
+                    case "public":
+                        this.AppendFields(fields.Where(f => f.IsPublic));
+                        break;
+                    case "all":
+                        this.AppendFields(fields);
+                        break;
+                    default:
+                        break;
+                }
+
+                inputCommand = Console.ReadLine();
+            }
+
+            return this.strBuilder.ToString().Trim();
+        }
+
+        private void AppendFields(IEnumerable<FieldInfo> fieldsCollection)
+        {
+            foreach (var field in fieldsCollection)
+            {
+                var accessMod = field.Attributes.ToString().ToLower();
+                if (accessMod.Equals("family"))
+                {
+                    accessMod = "protected";
+                }
+
+                this.strBuilder.AppendLine($"{accessMod} {field.FieldType.Name} {field.Name}");
+            }
+        }
+    }
+}
---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/04___Reflection and Attributes - Exercise_08_tasks/01___HarvFieldsV2/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace _01___Harvesting_Fields
+{
+    using System;
+    class StartUp
+    {
+        static void Main()
+        {
+            // this task is the same as previous just is implemented without dictionary
+            HarvestingFieldsTest resultOutput = new HarvestingFieldsTest();
+            // print the result on the console using Run Method
+            Console.WriteLine(resultOutput.Run());
+
+        }
+    }
+}
