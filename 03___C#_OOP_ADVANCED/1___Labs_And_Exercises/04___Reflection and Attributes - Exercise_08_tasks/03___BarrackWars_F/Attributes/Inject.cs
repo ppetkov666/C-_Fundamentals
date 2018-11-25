ee0d0735ab700878297577f60c92e91712891bfd
Petko Petkov <ptpetkov666@gmail.com>
2018-11-25 23:52:48 +0200

    add one  class and tommorow the whole exersise will be finished

---
+++ 03___C#_OOP_ADVANCED/1___Labs_And_Exercises/04___Reflection and Attributes - Exercise_08_tasks/03___BarrackWars_F/Attributes/Inject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03___BarrackWars_Factory.Attributes
+{
+    class Inject
+    {
+        [AttributeUsage(AttributeTargets.Field)]
+        public class InjectAttribute : Attribute
+        {
+        }
+    }
+}
