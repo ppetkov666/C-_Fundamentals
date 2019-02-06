2e92c5ba5a3a38c87c6344d35ebddaf97153bf20
Petko Petkov <ptpetkov666@gmail.com>
2019-02-06 12:16:08 +0200

    small corrections and explanations to task 9

--- 01___C#_Advanced/1___Labs_And_Exercises/04___Streams and Files - Exercise_9_tasks/9. HTTP Server/StartUp.cs
+++ 01___C#_Advanced/1___Labs_And_Exercises/04___Streams and Files - Exercise_9_tasks/9. HTTP Server/StartUp.cs
@@ -10,8 +10,8 @@
     class HTTP_Server
     {
         private const int PortNumber = 5666;
-        private const string SourcePath = "../Resources";
-        private const string ErrorPath = "../Resources/error.html";
+        private const string SourcePath = "../../../../Resources";
+        private const string ErrorPath = "../../../../Resources/error.html";
         static void Main(string[] args)
         {
             TcpListener tcpListener = new TcpListener(IPAddress.Any, PortNumber);
@@ -23,14 +23,17 @@
 
             while (true)
             {
+                // it takes the bytes from the network
                 using (NetworkStream networkStream = tcpListener
                     .AcceptTcpClient()
                     .GetStream())
                 {
-                    var request = new byte[4096];
-                    var readBytes = networkStream.Read(request, 0, request.Length);
+                    // we create a buffer
+                    byte[] request = new byte[4096];
+                    int readBytes = networkStream.Read(request, 0, request.Length);
 
-                    var reqData = Encoding
+                    // we take the info and convert it to string 
+                    string reqData = Encoding
                         .UTF8
                         .GetString(request, 0, readBytes)
                         .Replace("\\", "/");
@@ -38,21 +41,23 @@
 
                     if (!string.IsNullOrEmpty(reqData))
                     {
-                        var file = string.Empty;
+                        string file = string.Empty;
 
-                        var reqFirstLine = reqData
+                        // here we get the requet type, url and protocol and version and then from the array we extract it
+                        string[] reqFirstLine = reqData
                             .Substring(0, reqData.IndexOf(Environment.NewLine))
                             .Split();
-                        var url = reqFirstLine[1];
-                        var statusLine = $"{reqFirstLine[2]} 200 OK";
+                        string url = reqFirstLine[1];
+                        string statusLine = $"{reqFirstLine[2]} 200 OK";
 
+                        // if this is the default url we just assing to var file - our index page with the path which later will be displayd
                         if (url == "/")
                         {
                             file = $"{SourcePath}/index.html";
                         }
                         else
                         {
-                            var reqFile = $"{SourcePath}{url.Substring(url.IndexOf('/'))}";
+                            string reqFile = $"{SourcePath}{url.Substring(url.IndexOf('/'))}";
 
                             if (!reqFile.EndsWith(".html"))
                             {
@@ -69,18 +74,18 @@
                                 statusLine = "HTTP/1.0 404 Not Found";
                             }
                         }
-
-                        
-                        var responseHeader = new StringBuilder();
+                       // then here we create out response header pattern
+                        StringBuilder responseHeader = new StringBuilder();
                         responseHeader.Append($"{statusLine}{Environment.NewLine}");
                         responseHeader.Append($"Accept-Ranges: bytes{Environment.NewLine}");
+                       
+                        // we initialize the response and read it from a file which is our html pages
+                        // and append it to stringbuilder
+                        StringBuilder response = new StringBuilder();
 
-                        
-                        var response = new StringBuilder();
-
-                        using (var reader = new StreamReader(file))
+                        using (StreamReader reader = new StreamReader(file))
                         {
-                            var line = reader.ReadLine();
+                            string line = reader.ReadLine();
 
                             while (line != null)
                             {
@@ -95,22 +100,18 @@
                                 .Replace("{0}", DateTime.Now.ToString("dd MMM yyyy hh:mm:ss", new CultureInfo("en-EN")))
                                 .Replace("{1}", Environment.ProcessorCount.ToString());
                         }
-
                         
-                        var contentLingth = Encoding.UTF8.GetBytes(response.ToString()).Length;
-
-                        responseHeader.Append($"ContentLength: {contentLingth}{Environment.NewLine}");
+                        
+                        int contentLength = Encoding.UTF8.GetBytes(response.ToString()).Length;
+                        responseHeader.Append($"ContentLength: {contentLength}{Environment.NewLine}");
                         responseHeader.Append($"Connection: close{Environment.NewLine}");
                         responseHeader.Append($"Content-Type: text/html{Environment.NewLine}");
                         responseHeader.Append(Environment.NewLine);
-
-                        
-                        response.Insert(0, responseHeader);
-
-                        
+                        // then we insert our response header to the response if the right format 
+                        // after the header we have new line and after the new line it is the content which browser will render
+                        response.Insert(0, responseHeader);                  
+                        // and the final step is to convert it to byte array  and to send it using Write method
                         byte[] responseBytes = Encoding.UTF8.GetBytes(response.ToString());
-
-                        
                         networkStream.Write(responseBytes, 0, responseBytes.Length);
                     }
                 }
